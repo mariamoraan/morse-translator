7e3d3648c7606c8f946427d69795cfdf93a3fd84
Maria Moran <mariamoranluaces@gmail.com>
2024-06-28 20:26:14 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './core/styles/globals.css';
+import { RouterProvider } from 'react-router-dom';
+import { AppRouting } from './core/routing/app-routing';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={AppRouting} />
+  </React.StrictMode>,
+);
